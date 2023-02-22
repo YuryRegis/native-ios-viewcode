7388bdb64bdb3f47070499bfe5ccf34da131a1ff
YuryRegis <yury_regis@hotmail.com>
2023-02-22 17:43:15 -0300

    primeiro elemento

---
+++ viewCodeStudie/viewCodeStudie/Login/Screen/LoginScreen.swift
@@ -0,0 +1,25 @@
+//
+//  LoginScreen.swift
+//  viewCodeStudie
+//
+//  Created by Yury Regis on 22/02/23.
+//
+
+import UIKit
+
+class LoginScreen: UIView {
+    
+    lazy var loginLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
