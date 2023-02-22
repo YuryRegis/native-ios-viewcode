d80b4fde3e57e5dd1d4c437ab0399ac871ae0661
YuryRegis <yury_regis@hotmail.com>
2023-02-22 19:04:07 -0300

    adicionando textFields

--- viewCodeStudie/viewCodeStudie/Login/Screen/LoginScreen.swift
+++ viewCodeStudie/viewCodeStudie/Login/Screen/LoginScreen.swift
@@ -18,6 +18,37 @@ class LoginScreen: UIView {
         return label
     }()
     
+    lazy var emailTextField: UITextField = {
+        let email = UITextField()
+        email.textColor = .white
+        email.font = UIFont.systemFont(ofSize: 20)
+        email.backgroundColor = .darkGray
+        email.borderStyle = .roundedRect
+        email.layer.borderColor = UIColor.white.cgColor
+        email.layer.borderWidth = 1
+        email.layer.cornerRadius = 9
+        email.attributedPlaceholder = NSAttributedString(string: "exemplo@email.com", attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        email.keyboardType = .emailAddress
+        email.translatesAutoresizingMaskIntoConstraints = false
+        return email
+    }()
+    
+    lazy var passwordTextField: UITextField = {
+        let password = UITextField()
+        password.textColor = .white
+        password.attributedPlaceholder = NSAttributedString(string: "password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        password.isSecureTextEntry = true
+        password.font = UIFont.systemFont(ofSize: 20)
+        password.backgroundColor = .darkGray
+        password.borderStyle = .roundedRect
+        password.layer.borderWidth = 1
+        password.layer.cornerRadius = 9
+        password.layer.borderColor = UIColor.white.cgColor
+        password.keyboardType = .default
+        password.translatesAutoresizingMaskIntoConstraints = false
+        return password
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         addElements()
@@ -31,12 +62,22 @@ class LoginScreen: UIView {
     
     private func addElements() {
         self.addSubview(loginLabel)
+        self.addSubview(emailTextField)
+        self.addSubview(passwordTextField)
     }
     
     private func configConstraints() {
         NSLayoutConstraint.activate([
             loginLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
-            loginLabel.centerXAnchor.constraint(equalTo: centerXAnchor)
+            loginLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            emailTextField.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 50),
+            emailTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            emailTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15),
+            
+            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 15),
+            passwordTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            passwordTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15)
         ])
     }
 }
