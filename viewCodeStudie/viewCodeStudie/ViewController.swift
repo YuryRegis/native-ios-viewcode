379f235ff23ad5d19095144d03c080a34bf3f596
YuryRegis <yury_regis@hotmail.com>
2023-02-22 16:40:51 -0300

    first commit

---
+++ viewCodeStudie/viewCodeStudie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  viewCodeStudie
+//
+//  Created by Yury Regis on 22/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
